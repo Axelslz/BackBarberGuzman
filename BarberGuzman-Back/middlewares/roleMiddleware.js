5c9342e5e9d0e26b1005615a8485d4a57a36b3da
axelslz <axlsalazarr1602@gmail.com>
2025-08-21 14:49:01 -0600

    update93

--- BarberGuzman-Back/middlewares/roleMiddleware.js
+++ BarberGuzman-Back/middlewares/roleMiddleware.js
@@ -1,6 +1,6 @@
 const permitirRoles = (...roles) => {
     return (req, res, next) => {
-      if (!roles.includes(req.usuario.role)) {
+      if (!req.user || !roles.includes(req.user.role)) {
         return res.status(403).json({ mensaje: 'No tienes permisos para realizar esta acción' });
       }
       next();
