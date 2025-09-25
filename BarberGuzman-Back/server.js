fc8443eb332b67979c7a0bb1be59369ad04b008f
Axelslz <axlsalazarr1602@gmail.com>
2025-09-25 12:30:19 -0600

    update server

--- BarberGuzman-Back/server.js
+++ BarberGuzman-Back/server.js
@@ -8,16 +8,31 @@ const servicioRoutes = require('./routes/servicioRoutes');
 const errorMiddleware = require('./middlewares/errorMiddleware');
 const { startScheduler } = require('./utils/scheduler');
 const aboutRoutes = require('./routes/aboutRoutes');
-const chatBotRoutes = require('./routes/chatBotRoutes'); 
+const chatBotRoutes = require('./routes/chatBotRoutes');
 require('dotenv').config();
 const path = require('path');
-const bodyParser = require('body-parser'); 
+const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const allowedOrigins = [
+    'https://guzmanbarberweb.netlify.app',
+    'http://localhost:3000',               
+    'http://localhost:5173'                
+];
+
 const corsOptions = {
-  origin: 'https://guzmanbarberweb.netlify.app'
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('No permitido por la política de CORS'));
+        }
+    },
+   
+    credentials: true,
 };
+
 app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
@@ -34,11 +49,11 @@ app.use('/api/barberos', barberRoutes);
 app.use('/api/servicios', servicioRoutes);
 app.use('/api/citas', citasRoutes);
 app.use('/api/about', aboutRoutes);
-app.use('/api/chatbot', chatBotRoutes); 
+app.use('/api/chatbot', chatBotRoutes);
 
 app.use(errorMiddleware);
 
 app.listen(port, () => {
-    console.log(`Servidor corriendo en el puerto ${port}`);
-    startScheduler();
+    console.log(`Servidor corriendo en el puerto ${port}`);
+    startScheduler();
 });
